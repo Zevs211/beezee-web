2da1cab6769fd792b6ac0e4e7a4a8d8290936b81
Oleg Polyachenkov <jeffersoncity95@gmail.com>
2021-12-07 19:11:10 +0300

    initial promo page

--- src/pages/Determiner.vue
+++ src/pages/Determiner.vue
@@ -1,28 +1,194 @@
 <template>
-  <div class="w-full h-screen flex flex-col items-center justify-center bg-gray-100 bg-gradient-to-r from-blue-300 to-green-300">
-    <div class="h-2/5">
-      <div class="h-full flex items-end">
-        <h1 class=" text-6xl text-black font-extrabold">
-          I'm here to...
-        </h1>
+  <!-- Who are you -->
+  <div
+    v-if="characterClassIsEmpty"
+    class="w-full h-screen flex flex-col items-center justify-center"
+  >
+    <h1 class="text-6xl text-black font-extrabold">
+      Я здесь чтобы...
+    </h1>
+    <div class="w-full flex items-center justify-center mt-16">
+      <div
+        class="
+          flex flex-col items-center justify-center
+          rounded-xl
+          shadow
+          bg-green-200
+          py-16
+          px-8
+          mr-12
+          cursor-pointer select-none
+          transition-shadow transition-transform hover:shadow-xl transform hover:scale-105
+        "
+        @click="chooseClass('vendor')"
+      >
+        <div class="text-4xl text-black font-bold">Найти работу</div>
+        <div class="text-3xl text-black mt-4">в качестве фрилансера</div>
+      </div>
+      <div
+        class="
+          flex flex-col items-center justify-center
+          rounded-xl
+          shadow
+          bg-yellow-200
+          py-16
+          px-8
+          cursor-pointer select-none
+          transition-shadow transition-transform hover:shadow-xl transform hover:scale-105
+        "
+        @click="chooseClass('client')"
+      >
+        <div class="text-4xl text-black font-bold">Заказать проект</div>
+        <div class="text-3xl text-black mt-4">будучи клиентом</div>
       </div>
     </div>
-    <div class="w-full h-3/5 flex items-center">
-      <div class="w-1/2 h-full flex justify-end mr-10 mt-32 select-none">
-        <div class="bg-green-200 w-2/3 h-2/4 rounded-xl cursor-pointer flex flex-col items-center justify-center transition-shadow transition-transform hover:shadow-xl transform hover:scale-110">
-          <div class="text-4xl text-black font-bold">Make money</div>
-          <div class="text-3xl text-black mt-4">as a freelancer</div>
-        </div>
+  </div>
+  <!-- I'm a vendor -->
+  <div
+    v-if="isVendor"
+    class="w-full h-screen flex flex-col items-center justify-center"
+  >
+    <div class="w-full flex flex-col items-center">
+      <h1 class="text-6xl text-black font-extrabold">Что вы умеете?</h1>
+      <h4 class="text-2xl text-black mt-4">Перечислите ключевые навыки</h4>
+      <div class="w-1/3 mt-10">
+        <input
+          type="text"
+          class="w-full h-12 border border-gray-400 focus:border-black transition-colors rounded-xl px-4 outline-none text-xl bg-transparent"
+          v-model="skillInputValue"
+          @keypress="onInputKeyPress"
+          maxlength="30"
+          autofocus
+          tabindex="1"
+        >
       </div>
-      <div class="w-1/2 h-full flex justify-start ml-10 mt-32 select-none">
-        <div class="bg-yellow-200 w-2/3 h-2/4 rounded-xl cursor-pointer flex flex-col items-center justify-center transition-shadow transition-transform hover:shadow-xl transform hover:scale-110">
-          <div class="text-4xl text-black font-bold">Make a project</div>
-          <div class="text-3xl text-black mt-4">as a client</div>
-        </div>
+    </div>
+    <div class="w-3/5 flex justify-center flex-wrap mt-6">
+      <transition-group
+        enter-active-class="opacity-100 transform scale-125"
+        leave-active-class="opacity-0 transform scale-0"
+        appear
+        persisted
+      >
+        <span
+          v-for="(skill, index) in skills"
+          :key="index"
+          class="
+            h-12 border border-gray-400
+            rounded-xl flex items-center
+            text-xl px-4 bg-gray-100 cursor-pointer
+            relative mt-4
+            transition
+          "
+          :class="{ 'ml-4': index > 0 }"
+          :tabindex="index + 2"
+          @keydown="onSkillKeypress($event, skill)"
+        >
+          <span class="pr-4 transition-colors hover:text-blue-600">
+            {{ `#${skill}` }}
+          </span>
+          <IconClose
+            class="w-4 h-4 absolute top-2 right-2 transition-colors hover:text-red-600"
+            @click="removeSkill(skill)"
+          />
+        </span>
+      </transition-group>
+    </div>
+    <button
+      class="
+        h-10 border border-green-500 rounded-xl
+        px-4 mt-12 text-xl bg-green-300
+        transition-colors transition-opacity hover:bg-green-500
+      "
+      :disabled="!isEnoughSkills"
+      :class="{ 'opacity-30 cursor-not-allowed': !isEnoughSkills }"
+      :tabindex="skills.length + 3"
+      @click="onFindJob"
+    >Найти работу</button>
+  </div>
+  <!-- I'm a client -->
+  <div
+    v-if="isClient"
+    class="w-full h-screen flex flex-col items-center justify-center"
+  >
+    <div class="w-full flex flex-col items-center">
+      <h1 class="text-6xl text-black font-extrabold">У вас есть дизайн проекта?</h1>
+      <h4 class="text-2xl text-black mt-4">или любая другая сопутствующая документация</h4>
+    </div>
+    <div class="w-full flex items-center justify-center mt-16">
+      <div
+        class="
+          flex flex-col items-center justify-center
+          rounded-xl
+          shadow
+          bg-green-200
+          py-16
+          px-8
+          mr-12
+          cursor-pointer select-none
+          transition-shadow transition-transform hover:shadow-xl transform hover:scale-105
+        "
+        @click="openProjectCreation(true)"
+      >
+        <div class="text-4xl text-black font-bold">Да</div>
+        <div class="text-3xl text-black mt-4">есть дизайн или ТЗ</div>
+      </div>
+      <div
+        class="
+          flex flex-col items-center justify-center
+          rounded-xl
+          shadow
+          bg-yellow-200
+          py-16
+          px-8
+          cursor-pointer select-none
+          transition-shadow transition-transform hover:shadow-xl transform hover:scale-105
+        "
+        @click="openProjectCreation()"
+      >
+        <div class="text-4xl text-black font-bold">Нет</div>
+        <div class="text-3xl text-black mt-4">но у меня есть идея проекта</div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+import IconClose from '../components/icons/Close.vue'
+
+const characterClass = ref('')
+const skillInputValue = ref('')
+const skills = ref([])
+
+const characterClassIsEmpty = computed(() => !characterClass.value)
+const isVendor = computed(() => characterClass.value === 'vendor')
+const isClient = computed(() => characterClass.value === 'client')
+const isEnoughSkills = computed(() => skills.value.length > 0)
+
+const chooseClass = (className) => characterClass.value = className
+const onInputKeyPress = (payload) => {
+  if (payload.key === "Enter") processInputValue()
+}
+const processInputValue = () => {
+  const str = skillInputValue.value
+  if (str.length >= 30 || skills.value.includes(str)) return
+  if (str !== '') {
+    skills.value.push(str)
+  }
+  skillInputValue.value = ''
+}
+const removeSkill = (skill) => {
+  skills.value = skills.value.filter(s => s !== skill)
+}
+const onSkillKeypress = (e, skill) => {
+  if (e.key === "Backspace") removeSkill(skill)
+}
+const onFindJob = () => {
+  // redirect to project list with applied filter by selected skills
+}
+const openProjectCreation = (files) => {
+  // if files is true - open project creation page from files uploading
+  // if false - from constructor
+}
 </script>
